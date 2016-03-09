bb43be4e34818d0abaf8883ada5e6e0e4cccd87a
Roger Bisson <roger.bisson@citypay.com>
2016-03-09 11:22:11 +0000

    Updated version to 1.0.6.RELEASE

--- wp-content/plugins/citypay-paylink-woocommerce/citypay-paylink-woocommerce.php
+++ wp-content/plugins/citypay-paylink-woocommerce/citypay-paylink-woocommerce.php
@@ -3,7 +3,7 @@
 Plugin Name: CityPay WooCommerce Payments
 Plugin URI: http://www.citypay.com/
 Description: CityPay PayLink Payment Pages for WooCommerce
-Version: 1.0.4
+Version: 1.0.6
 Author: CityPay Limited
 Author URI: http://www.citypay.com/
 License: GPL2
