d53360d5cb94873180412bd62842c7d5c5b93cc5
hectorconceicao <hector.conceicao@citypay.com>
2023-01-11 08:47:21 +0000

    Changed t_ident_prefix description.

--- src/WC_Gateway_CityPay_Paylink.php
+++ src/WC_Gateway_CityPay_Paylink.php
@@ -158,7 +158,7 @@ class WC_Gateway_CityPayPaylink extends WC_Gateway_CityPay
             't_ident_prefix' => array(
                 'title' => __('Transaction identifier prefix', 'wc-payment-gateway-citypay'),
                 'type' => 'text',
-                'description' => __('A Identifier identifies a particular transaction linked to a Merchant account (Length 5-50 characters). Introduce a prefix at least with 4 characters.', 'wc-payment-gateway-citypay'),
+                'description' => __('A Identifier identifies a particular transaction linked to a Merchant account (Length 5-50 characters). The OrderID will be concatenated to the prefix. Introduce a prefix at least with 4 characters.', 'wc-payment-gateway-citypay'),
                 'default' => __('OrderID#', 'wc-payment-gateway-citypay'),
             ),
             'postback_base' => array(
